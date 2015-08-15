8d30fdb45a0690d093ec90e6bc6b3858a94744af
dabong2 <yoonique9012@gmail.com>
2015-08-15 16:33:13 +0900

    code 2097 update

--- week5/dabong2/A_2097.java
+++ week5/dabong2/A_2097.java
@@ -8,10 +8,8 @@ public class A_2097 {
 
 
     public static void main(String[] args) {
-      //  Scanner in = new Scanner(new File("input.txt"));
         Scanner in = new Scanner(System.in);
-      //  PrintWriter out = new PrintWriter(new File("output.txt"));
-        
+         
         int n = in.nextInt();
         Subway s = new Subway(n, in.nextInt());
         for(int i = 1; i <= n; ++i) {
